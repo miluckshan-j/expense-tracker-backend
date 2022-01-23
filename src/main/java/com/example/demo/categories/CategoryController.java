a7176390ac520992d08ea77b9ec21568bb89a80a
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-01-24 00:16:13 +0530

    remove unused imports

--- src/main/java/com/example/demo/categories/CategoryController.java
+++ src/main/java/com/example/demo/categories/CategoryController.java
@@ -1,12 +1,10 @@
 package com.example.demo.categories;
 
-import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 @RestController
